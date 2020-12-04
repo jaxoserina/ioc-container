e7addc2abea4d2484fbd0a50a063676106846a61
Jaxo Natarajan <jaxoserina1@gmail.com>
2020-12-04 00:18:58 +0000

    Unit tests for the Container class

---
+++ src/instances/container.spec.ts
@@ -0,0 +1,36 @@
+import { container, Container } from './container';
+import { find } from 'lodash';
+
+class MockTestService {
+  constructor () {
+
+  }
+};
+
+describe('container', () => {
+  let mockContainer: Container = new Container();
+
+  beforeEach(() => {
+    mockContainer.provide({
+      token: 'mockTestService',
+      useValue: MockTestService
+    });
+
+  });
+
+  it('should expect the providers object to be initialised', () => {
+    expect(mockContainer.providers).toBeDefined();
+  });
+
+  it("should resolve token for MockTestService and return the service registered", () => {
+    const matchedProvider = mockContainer.resolve('mockTestService');
+    expect(matchedProvider).toBe(MockTestService);
+  });
+
+  it('should throw an error when no matcing token found', () => {
+    expect(() => {
+      mockContainer.resolve('random');
+    }).toThrow('No provider found for random');
+  });
+
+});
